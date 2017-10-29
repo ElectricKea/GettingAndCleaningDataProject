da0981351f9ec4ab08f3698d74a5e1f59633229d
Kyle <kyle.mcmillan@gmail.com>
2017-10-29 14:58:04 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+#Getting and cleaning data course project
+
+Completed as part of the Getting and Cleaning Data course.
+The data was collected from accelerometers from the Samsung Galaxy S smartphone.
+Data downloaded from:
+<https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip>
+
+
+### Files included
+* run_analysis.R - R script to read, tidy and summerise the data.
+* CodeBook.md - listing of all variables used in the data. 
+* README.md - This file you're currently reading.
+* tidy_data.txt - The output file from run_analysis.R
+
+
+### How to
+1. Download the data from the link above. Keep the data in it's original "UCI HAR Dataset" format.
+2. Read through the comments in the run_analysis.R file to understand how the data is being tidied up.
+3. Read though the CodeBook.md to understand what variables are being used.
+4. Run the run_analysis.R file.
---
+++ run_analysis.R
@@ -0,0 +1,33 @@
+library(dplyr)
+
+#Read and load features for the data and remove untidy brackets from features data
+features<-gsub("\\(\\)","",as.vector(read.table("UCI HAR Dataset/features.txt")[,2]))
+activ_labels<-as.vector(read.table("UCI HAR Dataset/activity_labels.txt")[,2])
+
+# mean and standard diviation columns to keep
+col_nums<-c(1:5, 41:46, 81:86, 121:126, 161:166, 201, 202, 214, 215, 227, 228, 253,254, 266:271, 345:350, 424:429, 503, 504, 516, 517, 529, 530, 542, 543)
+
+#Read and load files
+Xtrain<-read.table("UCI HAR Dataset/train/X_train.txt")[col_nums]
+train_full<-cbind(read.table("UCI HAR Dataset/train/subject_train.txt"), read.table("UCI HAR Dataset/train/y_train.txt"), Xtrain)
+Xtest<-read.table("UCI HAR Dataset/test/X_test.txt")[col_nums]
+test_full<-cbind(read.table("UCI HAR Dataset/test/subject_test.txt"), read.table("UCI HAR Dataset/test/y_test.txt"), Xtest)
+
+#Combine training and testing data into one dataframe
+data_full<-rbind(train_full, test_full)
+colnames(data_full)<-c("subject", "activity", features[col_nums])
+
+#rename the variables for the activities to their original activity names
+for(number in c(1:6)){data_full$activity[data_full$activity == number]<-activ_labels[number]}
+
+#Remove no longer needed dataframes from the environment
+#rm(Xtest, Xtrain, test_full, train_full, features, activ_labels)
+
+#group the data by the subject and activity
+grouped_data<-group_by(data_full, subject, activity)
+
+#Average all the values for each variable for each subject for each activity
+grouped_mean_data<-summarise_all(grouped_data, funs(mean))
+
+#Write the grouped mean data to a text file in table format
+write.table(grouped_mean_data, "tidy_data.txt")
